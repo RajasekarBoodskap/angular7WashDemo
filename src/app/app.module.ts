93235575881e59cfd5dc67c45cafa9271fc6069a
RajasekarBoodskap <rajasekarp@boodskap.io>
2019-05-07 15:37:06 +0530

    add ngx smart table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule,  NbSidebarModule, NbSidebarService} from '@nebular/theme';
+import { ThemeModule } from './@theme/theme.module';
 import { CookieService } from 'ngx-cookie-service';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -14,6 +16,8 @@ import { FooterComponent } from './pages/footer/footer.component';
 import { SidebarHeaderComponent } from './pages/sidebar-header/sidebar-header.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { WashComponent } from './pages/wash/wash.component';
+
+
 
 @NgModule({
   declarations: [
@@ -31,9 +35,11 @@ import { WashComponent } from './pages/wash/wash.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule.forRoot(),
+    ThemeModule.forRoot(),
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
-    NbSidebarModule
+    NbSidebarModule,
+    Ng2SmartTableModule
     
   ],
   providers: [NbSidebarService, CookieService],
--- src/app/pages/wash/wash.component.ts
+++ src/app/pages/wash/wash.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-wash',
   templateUrl: './wash.component.html',
@@ -11,5 +12,63 @@ export class WashComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      firstName: {
+        title: 'First Name',
+        type: 'string',
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      },
+    },
+  };
+
+  // source: LocalDataSource = new LocalDataSource();
+
+  // constructor(private service: SmartTableData) {
+  //   const data = this.service.getData();
+  //   this.source.load(data);
+  // }
+
+  // onDeleteConfirm(event): void {
+  //   if (window.confirm('Are you sure you want to delete?')) {
+  //     event.confirm.resolve();
+  //   } else {
+  //     event.confirm.reject();
+  //   }
+  // }
 
 }
